46ab9c0d2ce18d2204b75a00d6808db6e19af835
mrym8980 <96958240+mrym8980@users.noreply.github.com>
2022-01-20 00:18:49 +0330

    fixed the bug of snake

--- game-core.cpp
+++ game-core.cpp
@@ -21,7 +21,7 @@ public:
 
         tileTexture.loadFromFile("pictures/blue2.jpg");
         snakeTexture.loadFromFile("pictures/sname.jpg",IntRect(1, 1, 45, 45));
-        appleTexture.loadFromFile("pictures/Untitled-1.psd");
+        appleTexture.loadFromFile("pictures/fish.psd");
         wallTexture.loadFromFile("pictures/stone.jpg");
 
         groundSprite.setTexture(tileTexture);
@@ -105,10 +105,10 @@ void checkCollision(RenderWindow& window, Info& info, Snake& snake1, Snake& snak
                 gameover(window, info);
 
     // pass border of the window
-    if (snake1.x[0] > info.height) {snake1.x[0] = 0;}
-    if (snake1.y[0] > info.width) {snake1.y[0] = 0;}
-    if (snake1.x[0] < 0) {snake1.x[0] = info.height;}
-    if (snake1.y[0] < 0) {snake1.y[0] = info.width;}
+    if (snake1.x[0] > info.height - 1) {snake1.x[0] = 0;}
+    if (snake1.y[0] > info.width - 1) {snake1.y[0] = 0;}
+    if (snake1.x[0] < 0) {snake1.x[0] = info.height - 1;}
+    if (snake1.y[0] < 0) {snake1.y[0] = info.width - 1;}
 
     // check collision with itself
     for (int i = 4; i < snake1.len; i++)
